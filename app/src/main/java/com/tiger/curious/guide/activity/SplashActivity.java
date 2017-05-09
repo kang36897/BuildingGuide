fe35995aa0b6aae7647af38dd26c07c37e26869b
Brian Kang <kang36897@gmail.com>
2017-05-09 20:35:27 +0800

    Initial commit

---
+++ app/src/main/java/com/tiger/curious/guide/activity/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.tiger.curious.guide.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.tiger.curious.guide.base.BaseActivity;
+
+/**
+ * Created by lulala on 9/5/2017.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/tiger/curious/guide/base/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.tiger.curious.guide.base;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lulala on 9/5/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    private  Handler mImmerseHandler = new Handler();
+
+
+
+
+
+}
