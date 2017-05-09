e2c5dea1a62d302089039fddbaffdd6436cb80e1
Brian Kang <kang36897@gmail.com>
2017-05-09 21:32:51 +0800

    start working

--- app/src/main/java/com/tiger/curious/guide/activity/SplashActivity.java
+++ app/src/main/java/com/tiger/curious/guide/activity/SplashActivity.java
@@ -1,9 +1,12 @@
 package com.tiger.curious.guide.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.tiger.curious.guide.R;
 import com.tiger.curious.guide.base.BaseActivity;
+import com.tiger.curious.guide.databinding.ActivitySplashBinding;
 
 /**
  * Created by lulala on 9/5/2017.
@@ -11,8 +14,11 @@ import com.tiger.curious.guide.base.BaseActivity;
 
 public class SplashActivity extends BaseActivity {
 
+    private ActivitySplashBinding mBinding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+
     }
 }
